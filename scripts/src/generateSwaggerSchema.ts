6f00cbe4a964997878e63cfb412b909af9dc38cf
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2024-07-25 16:15:44 +0200

    Added auto swagger fetch

---
+++ scripts/src/generateSwaggerSchema.ts
@@ -0,0 +1,23 @@
+import { exec } from "child_process"
+
+const url = "https://api.zwd.woon-o.azure.amsterdam.nl/api/schema/?format=json"
+
+exec(`dtsgen -o ./src/__generated__/apiSchema.d.ts --url ${ url }`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log("ERROR")
+      console.log(error.message)
+      process.exit(1)
+      return
+    }
+
+    if (stderr) {
+      console.log("ERROR")
+      console.log(stderr)
+      process.exit(1)
+      return
+    }
+
+    console.log(stdout)
+  }
+)
---
+++ src/__generated__/apiSchema.d.ts
@@ -0,0 +1,87 @@
+declare namespace Components {
+    namespace Schemas {
+        export interface Case {
+            id: number;
+            description: string;
+        }
+        export interface PatchedCase {
+            id?: number;
+            description?: string;
+        }
+    }
+}
+declare namespace Paths {
+    namespace ApiSchemaRetrieve {
+        namespace Parameters {
+            export type Format = "json" | "yaml";
+            export type Lang = "af" | "ar" | "ar-dz" | "ast" | "az" | "be" | "bg" | "bn" | "br" | "bs" | "ca" | "ckb" | "cs" | "cy" | "da" | "de" | "dsb" | "el" | "en" | "en-au" | "en-gb" | "eo" | "es" | "es-ar" | "es-co" | "es-mx" | "es-ni" | "es-ve" | "et" | "eu" | "fa" | "fi" | "fr" | "fy" | "ga" | "gd" | "gl" | "he" | "hi" | "hr" | "hsb" | "hu" | "hy" | "ia" | "id" | "ig" | "io" | "is" | "it" | "ja" | "ka" | "kab" | "kk" | "km" | "kn" | "ko" | "ky" | "lb" | "lt" | "lv" | "mk" | "ml" | "mn" | "mr" | "ms" | "my" | "nb" | "ne" | "nl" | "nn" | "os" | "pa" | "pl" | "pt" | "pt-br" | "ro" | "ru" | "sk" | "sl" | "sq" | "sr" | "sr-latn" | "sv" | "sw" | "ta" | "te" | "tg" | "th" | "tk" | "tr" | "tt" | "udm" | "ug" | "uk" | "ur" | "uz" | "vi" | "zh-hans" | "zh-hant";
+        }
+        export interface QueryParameters {
+            format?: Parameters.Format;
+            lang?: Parameters.Lang;
+        }
+        namespace Responses {
+            export interface $200 {
+                [name: string]: any;
+            }
+        }
+    }
+    namespace CasesCreate {
+        export type RequestBody = Components.Schemas.Case;
+        namespace Responses {
+            export type $201 = Components.Schemas.Case;
+        }
+    }
+    namespace CasesDestroy {
+        namespace Parameters {
+            export type Id = number;
+        }
+        export interface PathParameters {
+            id: Parameters.Id;
+        }
+        namespace Responses {
+            export interface $204 {
+            }
+        }
+    }
+    namespace CasesList {
+        namespace Responses {
+            export type $200 = Components.Schemas.Case[];
+        }
+    }
+    namespace CasesPartialUpdate {
+        namespace Parameters {
+            export type Id = number;
+        }
+        export interface PathParameters {
+            id: Parameters.Id;
+        }
+        export type RequestBody = Components.Schemas.PatchedCase;
+        namespace Responses {
+            export type $200 = Components.Schemas.Case;
+        }
+    }
+    namespace CasesRetrieve {
+        namespace Parameters {
+            export type Id = number;
+        }
+        export interface PathParameters {
+            id: Parameters.Id;
+        }
+        namespace Responses {
+            export type $200 = Components.Schemas.Case;
+        }
+    }
+    namespace CasesUpdate {
+        namespace Parameters {
+            export type Id = number;
+        }
+        export interface PathParameters {
+            id: Parameters.Id;
+        }
+        export type RequestBody = Components.Schemas.Case;
+        namespace Responses {
+            export type $200 = Components.Schemas.Case;
+        }
+    }
+}
