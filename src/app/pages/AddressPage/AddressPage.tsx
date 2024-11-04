bfa3e9671431f8fa5537b8dc7102127d485b9e48
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2024-11-04 13:11:56 +0100

    Added styling

--- src/app/pages/AddressPage/AddressPage.tsx
+++ src/app/pages/AddressPage/AddressPage.tsx
@@ -1,17 +1,61 @@
 import { HousingIcon } from "@amsterdam/design-system-react-icons"
-import { useParams } from "react-router-dom"
-import { PanoramaPreview, PageHeading } from "app/components"
+import { useNavigate, useParams } from "react-router-dom"
+import { PanoramaPreview, PageHeading, PageSpinner } from "app/components"
 import HoaDescription from "./HoaDescription"
+import { Button, Grid, GridColumnNumbers } from "@amsterdam/design-system-react"
+import { useHomeownerAssociationByBagId } from "app/state/rest"
+import { styled } from "styled-components"
+import HoaCases from "./HoaCases"
+import HoaOwners from "./HoaOwners"
 
+const gridSpan: GridColumnNumbers = { narrow: 4, medium: 8, wide: 6 }
+
+const Wrapper = styled.div`
+  margin-bottom: 40px;
+`
 
 export const AddressPage: React.FC = () => {
   const { bagId } = useParams<{ bagId: string }>()  
+  const [hoa, { isBusy }] = useHomeownerAssociationByBagId(bagId)
+  const navigate = useNavigate()
 
   return (
     <>
       <PageHeading label="Adresoverzicht" icon={ HousingIcon } />
-      { bagId && <PanoramaPreview bagId={ bagId } aspect={ 4 } fov={ 120 } />}
-      { bagId && <HoaDescription bagId={ bagId } /> }
+      { isBusy && <PageSpinner /> }
+      { !isBusy && bagId && (
+        <>
+          <Grid style={{ paddingLeft: 0 }} paddingTop="small" paddingBottom="large">
+            {hoa?.id ? ( 
+              <Grid.Cell span={ gridSpan } >
+                <HoaDescription hoa={ hoa } /> 
+              </Grid.Cell>
+            ) : (
+              <Grid.Cell span={ gridSpan } >
+                <p>Er zijn geen VVE-gegevens gevonden voor dit adres.</p>
+              </Grid.Cell>
+            )}
+            <Grid.Cell span={ gridSpan } >
+              <PanoramaPreview bagId={ bagId } aspect={ 4 } fov={ 120 } />
+            </Grid.Cell>
+          </Grid>
+          { hoa?.id && (
+            <>
+              <Wrapper>
+                <HoaOwners hoa={hoa} />
+              </Wrapper>
+              <Wrapper>
+                <HoaCases hoaId={hoa.id} />
+              </Wrapper>
+              <Wrapper>
+                <Button onClick={() => navigate(`/vve/${ hoa.id }/zaken/nieuw`)}>
+                  Nieuwe zaak aanmaken
+                </Button>
+              </Wrapper>
+            </>
+          )}
+        </>
+      )}
     </>
   )
 }
--- src/app/pages/AddressPage/HoaDescription.tsx
+++ src/app/pages/AddressPage/HoaDescription.tsx
@@ -1,53 +1,22 @@
-import { Button } from "@amsterdam/design-system-react"
-import { styled } from "styled-components"
-import { useNavigate } from "react-router-dom"
-import { PageSpinner, Descriptions } from "app/components"
-import { useHomeownerAssociationByBagId } from "app/state/rest"
-import HoaCases from "./HoaCases"
-import HoaOwners from "./HoaOwners"
+import { Descriptions } from "app/components"
 
 
 type Props = {
-  bagId: string
+  hoa: Components.Schemas.HomeownerAssociation
 }
 
-const Wrapper = styled.div`
-  margin: 24px 0;
-`
-
-export const HoaDescription: React.FC<Props> = ({ bagId }) => {
-  const [hoa, { isBusy }] = useHomeownerAssociationByBagId(bagId)
-  const navigate = useNavigate()
-
-  if (isBusy) {
-    return <PageSpinner />
-  }
-  if (hoa?.message) {
-    return <p>Er zijn geen VVE-gegevens gevonden voor dit adres.</p>
-  }
+export const HoaDescription: React.FC<Props> = ({ hoa }) => {
   if (hoa?.id) {
     const items = [
       { label: "VVE statutaire naam", children: hoa?.name },
-      { label: "Bouwjaar", children: hoa?.build_year },
-      { label: "Aantal woningen", children: hoa?.number_of_appartments },
+      { label: "Postcode", children: hoa?.zip_code },
+      { label: "Buurt", children: hoa?.neighborhood },
       { label: "Stadsdeel", children: hoa?.district },
-      { label: "Buurt", children: hoa?.neighborhood }
+      { label: "Bouwjaar", children: hoa?.build_year },
+      { label: "Aantal woningen", children: hoa?.number_of_appartments }
     ]
     return (
-      <>
-        <Wrapper>
-          <Descriptions items={ items } />
-        </Wrapper>
-        <Wrapper>
-          <HoaOwners hoa={ hoa } />
-        </Wrapper>
-        <Wrapper>
-          <HoaCases hoaId={ hoa.id } />
-        </Wrapper>
-        <Button onClick={ () => navigate(`/vve/${ hoa.id }/zaken/nieuw`)} >
-          Nieuwe zaak aanmaken  
-        </Button>
-      </>
+      <Descriptions items={ items } />
     )
   }
   return null
