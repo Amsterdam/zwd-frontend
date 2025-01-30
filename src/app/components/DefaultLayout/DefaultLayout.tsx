c113cccc6d5d7c79fd5ce5479195cd4b2e36e851
Niels Roozemond <n.roozemond@amsterdam.nl>
2025-01-30 13:09:32 +0100

    ADS packages update & Header menu change

--- src/app/components/DefaultLayout/DefaultLayout.tsx
+++ src/app/components/DefaultLayout/DefaultLayout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom"
-import { Header, SkipLink, PageMenu, Screen } from "@amsterdam/design-system-react"
-import { User, NavMenu } from "app/components"
+import {
+  Header,
+  SkipLink,
+  Screen,
+  LinkList
+} from "@amsterdam/design-system-react"
+import { User } from "app/components"
 import { env } from "app/config/env"
 import { useRedirectFromState } from "app/hooks"
 
+import styles from "./styles.module.css"
 
 export const DefaultLayout: React.FC = () => {
   useRedirectFromState()
@@ -13,15 +19,33 @@ export const DefaultLayout: React.FC = () => {
       <SkipLink href="#main">Direct naar inhoud</SkipLink>
       <Screen maxWidth="wide">
         <Header
-          appName={`${ env.VITE_APP_TITLE } ${ env.VITE_ENV }`}
-          links={(
-            <PageMenu alignEnd>
-              <li><User /></li>
-            </PageMenu>
-          )}
-        />
-        <NavMenu />
-        <br />
+          // eslint-disable-next-line template-curly-spacing
+          brandName={`${env.VITE_APP_TITLE} ${env.VITE_ENV}`}
+          className={styles.header}
+          menuItems={[
+            <Header.MenuLink key="BPMN" href="/bpmn">
+              BPMN
+            </Header.MenuLink>,
+            <Header.MenuLink key="Takenoverzicht" href="/taken">
+              Takenoverzicht
+            </Header.MenuLink>,
+            <Header.MenuLink key="Zakenoverzicht" href="/zaken">
+              Zakenoverzicht
+            </Header.MenuLink>,
+            <Header.MenuLink key="Zoeken" href="/">
+              Zoeken
+            </Header.MenuLink>,
+            <User key="User" />
+          ]}
+          noMenuButtonOnWideWindow
+        >
+          <LinkList className="ams-mb--lg">
+            <LinkList.Link href="/bpmn">BPMN</LinkList.Link>
+            <LinkList.Link href="/taken">Takenoverzicht</LinkList.Link>
+            <LinkList.Link href="/zakenoverzicht">Zakenoverzicht</LinkList.Link>
+            <LinkList.Link href="/">Zoeken</LinkList.Link>
+          </LinkList>
+        </Header>
         <main id="main">
           <Outlet />
         </main>
