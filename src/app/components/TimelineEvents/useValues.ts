aa72c4e08a633246896a969417237c3a39bc904e
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-03-11 22:49:29 +0100

    WON-145 Add DateInputField component and refactor useValues to improve event_variables mapping

--- src/app/components/TimelineEvents/useValues.ts
+++ src/app/components/TimelineEvents/useValues.ts
@@ -1,6 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { isValidDate, formatDate } from "app/utils/dates"
 import { EVENT_TYPES, getEventValueName } from "./dictionaries"
-
 
 type Value = {
   key: string
@@ -10,7 +10,20 @@ type Value = {
 export const useValues = (event: CaseEvent) => {
   const { event_values, event_variables } = event
   const values: Value[] = []
-  
+
+  // Map event_variables if a form has been submitted.
+  // There must be data (event_variables) and it cannot be a generic boolean (completed)
+  if (event_variables && !event_variables?.completed) {
+    Object.entries(event_variables).forEach(([, event]) => {
+      const eventValue = String(event?.value ?? "")
+      const value = isValidDate(eventValue)
+        ? formatDate(eventValue)
+        : eventValue
+      const key = String(event?.label ?? "")
+      values.push({ key, value })
+    })
+  }
+
   // Map event_values
   Object.entries(event_values).forEach(([key, value]) => {
     // Don't return the description of a GENERIC_TASK because this field is used as event title.
@@ -22,15 +35,6 @@ export const useValues = (event: CaseEvent) => {
     const keyValue = isValidDate(value) ? formatDate(value, true) : value
     values.push({ key: label, value: keyValue })
   })
-  
-  // Map event_variables if a form has been submitted. 
-  // There must be data (event_variables) and it cannot be a generic boolean (completed)
-  if (event_variables && !event_variables?.completed) {
-    Object.entries(event_variables).forEach(([, value]) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-      values.push({ key: value?.label, value: value?.value })
-    })  
-  }
-  
+
   return values
 }
