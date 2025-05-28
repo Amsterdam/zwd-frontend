cec4a9721eef441a7bcb4f151bd64906eeac3a91
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-05-28 14:07:51 +0200

    Changed indent

--- src/app/pages/CaseCreatePage/mapData.ts
+++ src/app/pages/CaseCreatePage/mapData.ts
@@ -16,26 +16,23 @@ const mapData = (
   advice_type: data.advice_type,
   homeowner_association,
   contacts: data?.fullname
-    ? [
-        {
-          fullname: data.fullname[0],
-          email: data.email[0],
-          phone: data.phone[0],
-          role:
-            data?.custom_role && data?.custom_role[0]
-              ? data?.custom_role[0]
-              : data.role[0]
-        },
-        {
-          fullname: data.fullname[1],
-          email: data.email[1],
-          phone: data.phone[1],
-          role:
-            data?.custom_role && data?.custom_role[1]
-              ? data?.custom_role[1]
-              : data.role[1]
-        }
-      ]
+    ? [{
+      fullname: data.fullname[0],
+      email: data.email[0],
+      phone: data.phone[0],
+      role:
+        data?.custom_role && data?.custom_role[0]
+          ? data?.custom_role[0]
+          : data.role[0]
+    }, {
+      fullname: data.fullname[1],
+      email: data.email[1],
+      phone: data.phone[1],
+      role:
+        data?.custom_role && data?.custom_role[1]
+          ? data?.custom_role[1]
+          : data.role[1]
+    }]
     : []
 })
 
