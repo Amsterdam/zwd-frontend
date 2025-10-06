cbd1a838224fc35ca6fbaee47e202286bbf98b7f
Koen Schaft <koen@schoenkaft.com>
2025-10-06 14:15:05 +0200

    Add dotenv and update generateSwaggerSchema to cater for local override

--- scripts/src/generateSwaggerSchema.ts
+++ scripts/src/generateSwaggerSchema.ts
@@ -1,7 +1,13 @@
 import { exec } from "child_process"
+import { config } from "dotenv"
 
-const url = "https://acc.api.zwd.amsterdam.nl/api/schema/?format=json"
-// const url = "http://localhost:8081/api/schema/?format=json"
+config({ path: ".env.local" })
+
+// To run against another backend than the default, add a `.env.local` file with the following content:
+// VITE_SWAGGER_SCHEMA_URL=http://localhost:8081/api/schema/?format=json
+const url = process.env.VITE_SWAGGER_SCHEMA_URL || "https://acc.api.zwd.amsterdam.nl/api/schema/?format=json"
+
+console.log(`👉 Generating schema from \`${ url }\``)
 
 exec(`dtsgen -o ./src/__generated__/apiSchema.d.ts --url ${ url }`,
   (error, stdout, stderr) => {
