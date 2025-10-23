692991fde3b83778ce17db4da8b4e0d2e85e2b98
Koen Schaft <koen@schoenkaft.com>
2025-10-23 11:26:55 +0200

    Add ColumnSettingsDialog and add it to ColumnSettings

--- src/app/components/ColumnSettings/ColumnSettings.tsx
+++ src/app/components/ColumnSettings/ColumnSettings.tsx
@@ -1,21 +1,29 @@
 import { Button } from "@amsterdam/design-system-react"
 import { TableIcon } from "@amsterdam/design-system-react-icons"
+import { useDialog } from "app/hooks"
+import ColumnSettingsDialog from "./ColumnSettingsDialog/ColumnSettingsDialog"
 
 export const ColumnSettings = ({ contextName }: { contextName: string }) => {
-
-  const handleClick = () => {
-    console.log(contextName)
-  }
+  const dialogId = "ams-dialog-column-settings"
+  const { openDialog, closeDialog } = useDialog(dialogId)
 
   return (
-    <Button
-      id="column-settings-button"
-      variant="secondary"
-      icon={<TableIcon />}
-      iconBefore
-      onClick={handleClick}
-    >
-      Kolomweergave
-    </Button>
+    <>
+      <Button
+        id="column-settings-button"
+        variant="secondary"
+        icon={<TableIcon />}
+        iconBefore
+        onClick={openDialog}
+      >
+        Kolomweergave
+      </Button>
+
+      <ColumnSettingsDialog
+        dialogId={dialogId}
+        contextName={contextName}
+        onClose={closeDialog}
+      />
+    </>
   )
 }
