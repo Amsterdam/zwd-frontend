c4d39f4f1cddf6537c949fb2a285394692e6eb84
Koen Schaft <koen@schoenkaft.com>
2025-12-17 10:15:39 +0100

    Adjust importer import instructions

--- src/app/pages/ImportPage/types/CourseParticipantImport.tsx
+++ src/app/pages/ImportPage/types/CourseParticipantImport.tsx
@@ -1,4 +1,4 @@
-import { Accordion, OrderedList, Paragraph } from "@amsterdam/design-system-react"
+import { Accordion, Paragraph, UnorderedList } from "@amsterdam/design-system-react"
 import type { FieldValues, UseFormReturn } from "react-hook-form"
 
 export const ImportInstructions: React.FC = () => (
@@ -6,28 +6,38 @@ export const ImportInstructions: React.FC = () => (
 
     <Accordion headingLevel={3}>
       <Accordion.Section label="Wat gebeurt er bij het importeren van cursusdeelnemers?">
-        <OrderedList>
-          <OrderedList.Item>
+        <UnorderedList>
+          <UnorderedList.Item>
             Bij een match met een vve worden bestaande contacten bijgewerkt met de cursusdatum uit het import-bestand, of toegevoegd als nieuw contact wanneer ze nog niet bestaan.
-          </OrderedList.Item>
-          <OrderedList.Item>
+          </UnorderedList.Item>
+          <UnorderedList.Item>
             Fouten tijdens het importeren worden na de import weergegeven.
-          </OrderedList.Item>
-          <OrderedList.Item>
+          </UnorderedList.Item>
+          <UnorderedList.Item>
             Een import kan altijd opnieuw worden uitgevoerd. Reeds geïmporteerde contacten worden niet opnieuw aangemaakt.
-          </OrderedList.Item>
-        </OrderedList>
+          </UnorderedList.Item>
+        </UnorderedList>
       </Accordion.Section>
     </Accordion>
 
     <Accordion headingLevel={3}>
       <Accordion.Section label="Welke kolommen moet het document bevatten?">
-        <Paragraph style={{ marginBottom: "1rem" }}>
+        <Paragraph>
           Het document moet de volgende kolommen bevatten:
         </Paragraph>
-        <OrderedList>
-          <OrderedList.Item><mark>Lorem</mark></OrderedList.Item>
-        </OrderedList>
+        <UnorderedList>
+          <UnorderedList.Item><strong>naam</strong>: Volledige naam van de cursusdeelnemer</UnorderedList.Item>
+          <UnorderedList.Item><strong>email</strong>: E-mailadres van de cursusdeelnemer</UnorderedList.Item>
+          <UnorderedList.Item><strong>cursusdatum</strong>: Cursusdatum</UnorderedList.Item>
+          <UnorderedList.Item><strong>vve</strong>: Statutaire naam van de vve</UnorderedList.Item>
+        </UnorderedList>
+        <Paragraph>
+          De volgende kolommen zijn optioneel:
+        </Paragraph>
+        <UnorderedList>
+          <UnorderedList.Item><strong>telefoon</strong>: Telefoonnummer van de cursusdeelnemer</UnorderedList.Item>
+          <UnorderedList.Item><strong>functie</strong>: Functie binnen de vve van de cursusdeelnemer</UnorderedList.Item>
+        </UnorderedList>
       </Accordion.Section>
     </Accordion>
 
--- src/app/pages/ImportPage/types/LetterImport.tsx
+++ src/app/pages/ImportPage/types/LetterImport.tsx
@@ -1,4 +1,4 @@
-import { Paragraph, OrderedList, Accordion } from "@amsterdam/design-system-react"
+import { Paragraph, Accordion, UnorderedList } from "@amsterdam/design-system-react"
 import { DateInputField, TextInputField, TextAreaField } from "app/components"
 import { validationRequired } from "app/utils/validation"
 import type { FieldValues, UseFormReturn } from "react-hook-form"
@@ -8,28 +8,28 @@ export const ImportInstructions: React.FC = () => (
 
     <Accordion headingLevel={3}>
       <Accordion.Section label="Wat gebeurt er bij het importeren van verzonden vve-brieven?">
-        <OrderedList>
-          <OrderedList.Item>
-            Bij een match met een vve wordt er een contactmelding aangemaakt. Deze is terug te vinden in het communicatieoverzicht van deze vve. Er wordt maar één contactmelding aangemaakt per vve, ongeacht hoe vaak deze voorkomt in het bronbestand.
-          </OrderedList.Item>
-          <OrderedList.Item>
+        <UnorderedList>
+          <UnorderedList.Item>
+            Bij een match met een vve wordt een contactmelding aangemaakt. Deze is terug te vinden in het communicatieoverzicht van deze vve. Er wordt maar één contactmelding aangemaakt per vve, ongeacht hoe vaak deze voorkomt in het bronbestand.
+          </UnorderedList.Item>
+          <UnorderedList.Item>
             Fouten tijdens het importeren worden na de import weergegeven.
-          </OrderedList.Item>
-          <OrderedList.Item>
+          </UnorderedList.Item>
+          <UnorderedList.Item>
             Een import kan altijd opnieuw worden uitgevoerd. Reeds aangemaakte contactmeldingen worden niet opnieuw aangemaakt, tenzij de gekozen verzenddatum van de brief verschilt van de vorige import.
-          </OrderedList.Item>
-        </OrderedList>
+          </UnorderedList.Item>
+        </UnorderedList>
       </Accordion.Section>
     </Accordion>
 
     <Accordion headingLevel={3}>
       <Accordion.Section label="Welke kolommen moet het document bevatten?">
-        <Paragraph style={{ marginBottom: "1rem" }}>
+        <Paragraph>
           Het document moet de volgende kolommen bevatten:
         </Paragraph>
-        <OrderedList>
-          <OrderedList.Item><strong>vve</strong>: Statutaire naam van de vve</OrderedList.Item>
-        </OrderedList>
+        <UnorderedList>
+          <UnorderedList.Item><strong>vve</strong>: Statutaire naam van de vve</UnorderedList.Item>
+        </UnorderedList>
       </Accordion.Section>
     </Accordion>
 
